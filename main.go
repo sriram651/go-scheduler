02ca55d603467a6e7724feee7530ed9a07e8e5fc
Sriram Venkatesh <vsriram353@gmail.com>
2026-02-20 12:08:23 +0530

    feat: phase 1 scheduler with skip-on-overlap and mutex guard"

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+var interval int
+var running bool
+var workerMutex sync.Mutex
 
 func main() {
-	fmt.Println("Starting scheduler...")
+	// Accept --interval flag
+	flag.IntVar(&interval, "interval", 5, "The time interval between each schedule in seconds")
+
+	flag.Parse()
+
+	if interval <= 1 {
+		fmt.Println("Interval should atleast be 2s")
+		os.Exit(2)
+	}
+
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+
+	for range ticker.C {
+		// Lock the access to running while checking
+		workerMutex.Lock()
+		if running {
+			workerMutex.Unlock()
+			fmt.Println(time.Now().Format("15:04:05"), "Skipping")
+			continue
+		}
+
+		running = true
+
+		// Access to next loop should be granted, so unlocking here
+		workerMutex.Unlock()
+
+		go func() {
+			doSomething()
+
+			// Since we access/modify this again, we do the lock & unlock again inside goroutine
+			workerMutex.Lock()
+			running = false
+			workerMutex.Unlock()
+		}()
+	}
+}
+
+func doSomething() {
+	fmt.Println(time.Now().Format("15:04:05"), "Doing")
+
+	time.Sleep(time.Duration(3) * time.Second)
+	fmt.Println("Finished")
 }
