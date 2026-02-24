5993777898fc36e8ea8db2bf11bc115fc358bbb6
Sriram Venkatesh <vsriram353@gmail.com>
2026-02-24 16:23:23 +0530

    refactor: enhance Telegram message sending with context handling and error management

--- sendMessage.go
+++ sendMessage.go
@@ -2,83 +2,77 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
+	"strings"
 )
-
-type EnvVars struct {
-	BotToken           string `json:"BOT_TOKEN"`
-	ChatId             string `json:"CHAT_ID"`
-	TelegramApiBaseUrl string `json:"TELEGRAM_API_BASE_URL"`
-}
-
-var BOT_TOKEN string
-var CHAT_ID string
-var TELEGRAM_API_BASE_URL string
-var sendMessagePath = "/sendMessage"
-
-var message string
-
-func init() {
-	godotenv.Load()
-
-	BOT_TOKEN = os.Getenv("BOT_TOKEN")
-	CHAT_ID = os.Getenv("CHAT_ID")
-	TELEGRAM_API_BASE_URL = os.Getenv("TELEGRAM_API_BASE_URL")
-}
 
 type SendMessage struct {
 	ChatId string `json:"chat_id"`
 	Text   string `json:"text"`
 }
 
-func SendTelegramMessage() {
-	flag.StringVar(&message, "message", "", "The message to be sent by the bot to the user.")
-	flag.StringVar(&message, "m", "", "The message to be sent by the bot to the user.")
-
-	flag.Parse()
-
-	if len(message) < 2 {
-		fmt.Println("Message needs to be atleast 2 characters...")
-		os.Exit(2)
-	}
-
-	endpoint := TELEGRAM_API_BASE_URL + BOT_TOKEN + sendMessagePath
-
+func (tc *TelegramClient) Send(ctx context.Context, text string) error {
 	message := SendMessage{
-		ChatId: CHAT_ID,
-		Text:   message,
+		ChatId: tc.chatId,
+		Text:   text,
 	}
 
 	messageJson, marshalErr := json.Marshal(message)
 
 	if marshalErr != nil {
-		fmt.Println(marshalErr)
-		os.Exit(2)
+		return marshalErr
 	}
 
 	sendMessageBody := bytes.NewBuffer(messageJson)
 
-	res, err := http.Post(endpoint, "application/json", sendMessageBody)
+	httpRequest, requestErr := http.NewRequestWithContext(ctx, http.MethodPost, tc.endpoint, sendMessageBody)
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+	if requestErr != nil {
+		return requestErr
 	}
 
-	body, readBodyErr := io.ReadAll(res.Body)
-	defer res.Body.Close()
+	httpRequest.Header.Set("Content-Type", "application/json")
 
-	if readBodyErr != nil {
-		fmt.Println(readBodyErr)
-		os.Exit(2)
+	response, responseErr := tc.client.Do(httpRequest)
+
+	if responseErr != nil {
+		return responseErr
 	}
 
-	fmt.Println("Here is the response:", string(body))
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(response.Body)
+		return fmt.Errorf("upstream error sending message %d: %s", response.StatusCode, strings.TrimSpace(string(body)))
+	}
+
+	var raw struct {
+		Ok     bool `json:"ok"`
+		Result struct {
+			MessageID int `json:"message_id"`
+			Chat      struct {
+				ID        int64  `json:"id"`
+				FirstName string `json:"first_name"`
+			} `json:"chat"`
+			Text string `json:"text"`
+		} `json:"result"`
+	}
+
+	responseDecoder := json.NewDecoder(response.Body)
+
+	decodeErr := responseDecoder.Decode(&raw)
+
+	if decodeErr != nil {
+		return decodeErr
+	}
+
+	result := "\nYou sent " + raw.Result.Chat.FirstName + " a message: \"" + raw.Result.Text + "\"\n"
+
+	fmt.Println(result)
+
+	return nil
 }
