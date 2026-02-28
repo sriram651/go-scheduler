cab08a40960e8df6c511ab4d2d10f0c733e5df1a
Sriram Venkatesh <vsriram353@gmail.com>
2026-02-28 15:19:19 +0530

    refactor: set cron location to local time for accurate scheduling

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/robfig/cron/v3"
 )
@@ -20,7 +21,7 @@ func New(schedule string) *Scheduler {
 
 func (s *Scheduler) Start(ctx context.Context, job func()) {
 	// Start the jobs here...
-	c := cron.New()
+	c := cron.New(cron.WithLocation(time.Local))
 
 	c.AddFunc(s.Schedule, job)
 
