33e65c1f5e96113dd46b6f334d994ebcf1dab681
Sriram Venkatesh <vsriram353@gmail.com>
2026-03-01 14:08:07 +0530

    feat: enhance logging for database connection and configuration loading

--- internal/db/db.go
+++ internal/db/db.go
@@ -9,17 +9,21 @@ import (
 )
 
 func Connect(dbURL string) *sql.DB {
+	log.Println("⏳Connecting to Postgres database...")
 	pgDB, err := sql.Open("pgx", dbURL)
 
 	if err != nil {
-		log.Fatalln("Connection to Postgres failed:", err)
+		log.Fatalln("❌Connection to Postgres failed:", err)
 	}
 
+	log.Println("⏳Pinging Postgres database...")
 	pingErr := pgDB.PingContext(context.Background())
 
 	if pingErr != nil {
-		log.Fatalln("Ping to Postgres failed:", pingErr)
+		log.Fatalln("❌Ping to Postgres failed:", pingErr)
 	}
+
+	log.Println("✅Connection to Postgres database established")
 
 	return pgDB
 }
